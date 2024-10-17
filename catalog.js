6f5d35caf6352ce757ed72738231047dfec6435c
Zachary Warns <warnsz@augsburg.edu>
2024-10-17 10:45:47 -0500

    added the catalog functions

--- catalog.js
+++ catalog.js
@@ -1,5 +1,15 @@
 window.addEventListener("DOMContentLoaded", connect);
-
+window.addEventListener("DOMContentLoaded" , addAllPets);
+var Pet = {
+    name : "smelly" ,
+    months : 37 ,
+    pounds : 60 ,
+    animal : "dog" ,
+    breed : ["germanshepard" ,"husky"] ,
+    traits : ["smelly" , "smart" , "playful" , "white" , "loyal"],
+    link : "https://example.org",
+    image : "Dog.jpeg",
+};
 
 
 function connect(){
@@ -14,5 +24,55 @@ function pageDown(){
     if(Number(document.getElementById("pageNumber").textContent) > 1){
         document.getElementById("pageNumber").textContent =  Number(document.getElementById("pageNumber").textContent) - 1;
     }
+}
+function addAllPets(){
+    addPet(Pet);
+}
+
+function addPet(pet){
+    var catalog = document.getElementsByClassName("Catalog")[0];
+    var newPet = document.createElement("div");
+    catalog.appendChild(newPet);
+    newPet.setAttribute("class" , "pet");
+    addCheckButton(newPet);
+    addImage(newPet , pet.image , pet.link);
+    addName(newPet , pet.name );
+    addAge(newPet , pet.months);
+}
+function addCheckButton(container){
+    let button = document.createElement("button");
+    container.appendChild(button);
+    let checkMark = document.createElement("img");
+    checkMark.setAttribute("src" , "checkmark.png");
+    checkMark.setAttribute("alt" , "a green checkmark");
+    button.appendChild(checkMark);
+}
+function addImage(container , image , link){
+    let a = document.createElement("a");
+    a.setAttribute("href" , link);
+    container.appendChild(a);
+    let img = document.createElement("img");
+    img.setAttribute("src" , image);
+    img.setAttribute("alt" , "adoptable animal");
+    a.appendChild(img);
+}
+function addName(container , name){
+    let nameDiv = document.createElement("div");
+    nameDiv.setAttribute("class" , "name");
+    nameDiv.textContent = name;
+    container.appendChild(nameDiv);
+}
+function addAge(container , months){
+    let ageDiv = document.createElement("div");
+    ageDiv.setAttribute("class" , "age");
+    let age = "";
+    if(months > 18){
+        age = Math.floor(months / 12) +" years";
+    }
+    else{
+        age = months +" months"
+    }
+    ageDiv.textContent = age;
+    container.appendChild(ageDiv);
 }
 
