7ee985a29294038a044756dd63353bb8d97cb6a3
Torrescc <torrescc@augsburg.edu>
2024-12-12 13:55:00 -0600

    Final Commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000; // You can change the port number if needed
+const PORT = 3809; // You can change the port number if needed
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
